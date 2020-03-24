03e0431e547fb656fe9e6df257a1d3a162f85aa6
Glendon Philipp Baculio <gpbaculio@gmail.com>
2020-03-24 22:14:35 +0800

    added illustrations and steps with animation

--- components/Block.tsx
+++ components/Block.tsx
@@ -1,28 +1,64 @@
 import React from 'react';
+import {Animated} from 'react-native';
+import {FlattenSimpleInterpolation} from 'styled-components';
 import styled, {css} from 'styled-components/native';
+
+import {colors} from '../constants/theme';
 
 interface BlockViewProps {
   center?: boolean;
   bot?: boolean;
   flex?: number;
+  middle?: boolean;
+  row?: boolean;
+  animated?: boolean;
+  defaultStyle?: FlattenSimpleInterpolation;
+  color?: 'gray';
+  animatedStyle?: {[animatedKey: string]: Animated.AnimatedInterpolation};
 }
 
 const BlockView = styled.View<BlockViewProps>`
   flex: 1;
   ${(props) =>
-    css`
+    css` 
       ${props.center && 'align-items: center;'}
       ${props.bot && 'justify-content: flex-end;'}
       ${props.flex && `flex:${props.flex}`}
+      ${props.middle && 'justify-content: center'}
+      ${props.row && 'flex-direction: row'}
+      ${props.defaultStyle && props.defaultStyle}
+      ${props.color && `background-color: ${colors[props.color]}`}
     `}
 `;
 
 interface BlockProps extends BlockViewProps {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 }
 
-const Block: React.FC<BlockProps> = ({children, center, bot, flex}) => (
-  <BlockView {...{center, bot, flex}}>{children}</BlockView>
-);
+const Block: React.FC<BlockProps> = ({
+  children,
+  center,
+  bot,
+  flex,
+  middle,
+  row,
+  defaultStyle,
+  animated,
+  color,
+  animatedStyle,
+}) => {
+  const blockViewStyles = {center, bot, flex, middle, row, defaultStyle, color};
+  if (animated) {
+    const AnimatedBlockView = Animated.createAnimatedComponent(BlockView);
+    return children ? (
+      <AnimatedBlockView {...blockViewStyles} style={animatedStyle}>
+        {children}
+      </AnimatedBlockView>
+    ) : (
+      <AnimatedBlockView {...blockViewStyles} style={animatedStyle} />
+    );
+  }
+  return <BlockView {...blockViewStyles}>{children}</BlockView>;
+};
 
 export default Block;
--- screens/Welcome.tsx
+++ screens/Welcome.tsx
@@ -1,14 +1,84 @@
 import React, {useState} from 'react';
-import {Text, View, Animated, Modal} from 'react-native';
+import {Text, View, Animated, Dimensions, Modal, Image} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 import {css} from 'styled-components';
 
 import Block from '../components/Block';
 import TextTypography from '../components/TextTypography';
 import {sizes} from '../constants/theme';
 
+const {width, height} = Dimensions.get('window');
 const Welcome = () => {
   const [showTerms, setShowTerms] = useState(false);
+  const illustrations = [
+    {id: 1, source: require('../assets/images/illustration_1.png')},
+    {id: 2, source: require('../assets/images/illustration_2.png')},
+    {id: 3, source: require('../assets/images/illustration_3.png')},
+  ];
   const scrollX = new Animated.Value(0);
+  const renderIllustrations = () => (
+    <FlatList
+      horizontal
+      pagingEnabled
+      scrollEnabled
+      showsHorizontalScrollIndicator={false}
+      scrollEventThrottle={16}
+      snapToAlignment="center"
+      data={illustrations}
+      extraData={showTerms}
+      keyExtractor={(item, index) => `${item.id}`}
+      renderItem={({item}) => (
+        <Image
+          source={item.source}
+          resizeMode="contain"
+          style={{width, height: height / 2, overflow: 'visible'}}
+        />
+      )}
+      onScroll={Animated.event([
+        {
+          nativeEvent: {contentOffset: {x: scrollX}},
+        },
+      ])}
+    />
+  );
+  const renderSteps = () => {
+    const stepPosition = Animated.divide(scrollX, width);
+    return (
+      <Block
+        row
+        center
+        middle
+        defaultStyle={css`
+          position: absolute;
+          bottom: ${sizes.base * 3}px;
+          right: 0;
+          left: 0;
+        `}>
+        {illustrations.map((item, index) => {
+          const opacity = stepPosition.interpolate({
+            inputRange: [index - 1, index, index + 1],
+            outputRange: [0.4, 1, 0.4],
+            extrapolate: 'clamp',
+          });
+
+          return (
+            <Block
+              animated
+              key={`step-${index}`}
+              color="gray"
+              defaultStyle={css`
+                width: 5px;
+                height: 5px;
+                border-radius: 5px;
+                margin-horizontal: 2.5px;
+              `}
+              animatedStyle={{opacity}}
+            />
+          );
+        })}
+      </Block>
+    );
+  };
   return (
     <Block>
       <Block center bot flex={0.4}>
@@ -27,6 +97,10 @@ const Welcome = () => {
           `}>
           Enjoy the experience.
         </TextTypography>
+      </Block>
+      <Block center middle>
+        {renderIllustrations()}
+        {renderSteps()}
       </Block>
     </Block>
   );
